3e99e005f5fdca334190691393ffe3d3d5748e3b
Hii One <hi@hii.one>
2022-05-27 12:08:39 +0700

    #14

--- scripts/minter.js
+++ scripts/minter.js
@@ -1,3 +1,114 @@
+function createMetadata(bgColor,fgColor) {
+
+	var time = Date.now();
+
+	var svgURI = document.getElementById("svg-preview").src;
+	var fontWeight = Number(getFontWeight("fontWeight"));
+
+	var uriObj = {};
+
+	uriObj.name = "Hi!@" + time;
+	uriObj.description = "On-chain SVG Hi! Simply customized & minted on https://hii.one";
+	uriObj.image = svgURI;
+	uriObj.external_url = "https://hii.one";
+
+	var fontWeight = {
+		trait_type: "Font Weight",
+		value: fontWeight
+	};
+
+	var fgColorObj = {
+		trait_type: "Color: Text",
+		value: fgColor
+	};
+
+	var bgColorObj = {
+		trait_type: "Color: Background",
+		value: bgColor
+	};
+
+	uriObj.attributes = [ fontWeight, fgColorObj, bgColorObj ];
+
+	var animator = document.getElementById('animation');
+	if (animator.checked) {
+		var feature1Obj = {
+			trait_type: "Feature",
+			value: "Color Animation"
+		};
+		uriObj.attributes.push(feature1Obj);
+	}
+	
+	var metadata = JSON.stringify(uriObj, null, 2);
+	console.log(metadata);
+	return metadata;
+}
+
+function createTokenUri(bgColor,fgColor) {
+	var metadata = createMetadata(bgColor,fgColor);
+	var uri = 'data:application/json;base64,' + btoa(metadata);
+	//console.log(uri);
+	return uri;
+}
+
+function setPrice(basePrice,tokenId,tokenBalance,account,owner) {
+	var price = basePrice * 100;
+	if (account === owner) {
+		price = 0;
+		console.log('Owner minting price:', price);
+	}
+	else if (!animationBool()) {
+		if (tokenId >=1 && tokenId <= 300) {
+			if (tokenBalance == 0) {
+				price = 0;
+				console.log('Your newly minting price:', price);
+			}
+			else {
+				price = basePrice;
+				console.log('Your next minting price:', price);
+			}
+		}
+		else if (tokenId >=301 && tokenId <= 1000) {
+			price = basePrice;
+			console.log('Tier 2 minting price:', price);
+		}
+		else if (tokenId >=1001 && tokenId <= 3000) {
+			price = basePrice * 10;
+			console.log('Tier 3 minting price:', price);
+		}
+		else {
+			console.log('Minting price:', price);
+		}
+	}
+	else {
+		if (tokenId >=1 && tokenId <= 300) {
+			if (tokenBalance == 0) {
+				price = basePrice;
+				console.log('Your newly animation minting price:', price);
+			}
+			else {
+				price = basePrice * 2;
+				console.log('Your next animation minting price:', price);
+			}
+		}
+		else if (tokenId >=301 && tokenId <= 1000) {
+			price = basePrice * 2;
+			console.log('Tier 2 animation minting price:', price);
+		}
+	
+		else if (tokenId >=1001 && tokenId <= 3000) {
+			price = basePrice * 10 * 2;
+			console.log('Tier 3 animation minting price:', price);
+		}
+		else {
+			price = basePrice * 100 * 2;
+			console.log('Animation minting price:', price);
+		}
+	}
+	//console.log('Minting price:', price);
+	return price;
+}
+
+
 function createLoginButton() {
 	var button = document.createElement("button");
     button.classList.add("login");
--- scripts/preview.js
+++ scripts/preview.js
@@ -61,6 +61,12 @@ function createSVG(bgColor,fgColor) {
 	return svg;
 }
 
+function createSvgUri(svg) {
+	var svgURI = 'data:image/svg+xml;base64,' + btoa(svg);
+	console.log(svgURI);
+	return svgURI;
+}
+
 function insertSVG(svg,holder) {
 	if(holder.innerHTML){
 		holder.innerHTML = '';
@@ -78,10 +84,9 @@ function insertSVG(svg,holder) {
 function placeSVG(bgColor,fgColor,holder) {
 	var svg = createSVG(bgColor,fgColor);
 	insertSVG(svg,holder);
-	//console.log("Animation:", animationBool());
 }
 
-function preview() {
+async function preview() {
 
 	var bgColor = randomColor();
 	var fgColor = randomColor();
@@ -95,11 +100,15 @@ function preview() {
 	console.log("Background color:", bgColor);
 	console.log("Foreground color:", fgColor);
 
+	console.log("Animation:", animationBool());
+
 	// color animation
 	var animator = document.getElementById('animation');
-	placeSVG(bgColor,fgColor,holder);
+	//placeSVG(bgColor,fgColor,holder);
+	
 	animator.addEventListener('change', function(event){
 		placeSVG(bgColor,fgColor,holder);
+		console.log("Animation:", animationBool());
 	});
 
 	// font weight selector
@@ -107,6 +116,9 @@ function preview() {
     var items = weightSelector.getElementsByTagName('option');
     var index = Math.floor(Math.random() * items.length); // random weight
     weightSelector.selectedIndex = index;
+	var fontWeight = weightSelector.value;
+	console.log("Font Weight:", fontWeight);
+
 	placeSVG(bgColor,fgColor,holder);
 
 	weightSelector.addEventListener('change', function(event){
@@ -129,122 +141,6 @@ function preview() {
 		console.log("Foreground color:", fgColor);
 	});
 
-}
-
-function createSvgUri(svg) {
-	var svgURI = 'data:image/svg+xml;base64,' + btoa(svg);
-	//console.log(svgURI);
-	return svgURI;
-}
-
-function createMetadata(bgColor,fgColor) {
-
-	var time = Date.now();
-
-	var svgURI = document.getElementById("svg-preview").src;
-	var fontWeight = Number(getFontWeight("fontWeight"));
-
-	var uriObj = {};
-
-	uriObj.name = "Hi!@" + time;
-	uriObj.description = "On-chain SVG Hi! Simply customized & minted on https://hii.one";
-	uriObj.image = svgURI;
-	uriObj.external_url = "https://hii.one";
-
-	var fontWeight = {
-		trait_type: "Font Weight",
-		value: fontWeight
-	};
-
-	var fgColorObj = {
-		trait_type: "Color: Text",
-		value: fgColor
-	};
-
-	var bgColorObj = {
-		trait_type: "Color: Background",
-		value: bgColor
-	};
-
-	uriObj.attributes = [ fontWeight, fgColorObj, bgColorObj ];
-
-	var animator = document.getElementById('animation');
-	if (animator.checked) {
-		var feature1Obj = {
-			trait_type: "Feature",
-			value: "Color Animation"
-		};
-		uriObj.attributes.push(feature1Obj);
-	}
-	
-	var metadata = JSON.stringify(uriObj, null, 2);
-	console.log(metadata);
-	return metadata;
-}
-
-function createTokenUri(bgColor,fgColor) {
-	var metadata = createMetadata(bgColor,fgColor);
-	var uri = 'data:application/json;base64,' + btoa(metadata);
-	//console.log(uri);
-	return uri;
-}
-
-function setPrice(basePrice,tokenId,tokenBalance,account,owner) {
-	var price = basePrice * 100;
-	if (account === owner) {
-		price = 0;
-		console.log('Owner minting price:', price);
-	}
-	else if (!animationBool()) {
-		if (tokenId >=1 && tokenId <= 300) {
-			if (tokenBalance == 0) {
-				price = 0;
-				console.log('Your newly minting price:', price);
-			}
-			else {
-				price = basePrice;
-				console.log('Your next minting price:', price);
-			}
-		}
-		else if (tokenId >=301 && tokenId <= 1000) {
-			price = basePrice;
-			console.log('Tier 2 minting price:', price);
-		}
-		else if (tokenId >=1001 && tokenId <= 3000) {
-			price = basePrice * 10;
-			console.log('Tier 3 minting price:', price);
-		}
-		else {
-			console.log('Minting price:', price);
-		}
-	}
-	else {
-		if (tokenId >=1 && tokenId <= 300) {
-			if (tokenBalance == 0) {
-				price = basePrice;
-				console.log('Your newly animation minting price:', price);
-			}
-			else {
-				price = basePrice * 2;
-				console.log('Your next animation minting price:', price);
-			}
-		}
-		else if (tokenId >=301 && tokenId <= 1000) {
-			price = basePrice * 2;
-			console.log('Tier 2 animation minting price:', price);
-		}
-	
-		else if (tokenId >=1001 && tokenId <= 3000) {
-			price = basePrice * 10 * 2;
-			console.log('Tier 3 animation minting price:', price);
-		}
-		else {
-			price = basePrice * 100 * 2;
-			console.log('Animation minting price:', price);
-		}
-	}
-	//console.log('Minting price:', price);
-	return price;
 }
 
 // Main display function
